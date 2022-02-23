a22acdeb85b79bd5a20f80de7ca106811e4708f0
KrouZ_CZ <64690903+KvantPro@users.noreply.github.com>
2022-02-23 23:40:11 +0300

    Update WG.py

--- WG.py
+++ WG.py
@@ -2,22 +2,19 @@ class WordlistGenerator():
     def __init__(self, leght, words = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"):
         self.words = words
         self.leght = leght
-        self.password = list("0" * self.leght)
-        self.len_passord = len(self.words) ** self.leght
-        self.i = self.leght - 1
+        self.password = [0 for i in range(self.leght)]
+        self.len_password = len(self.words) ** self.leght
         self.Programm = True
     def next(self):
-        while self.i >= 0:
-            if self.i != 0:
-                if int(self.password[self.i]) >= len(self.words):
-                    self.password[self.i-1] = int(self.password[self.i-1]) + 1
-                    self.password[self.i] = 0
+        if self.Programm == False: return
+        self.i = len(self.password) - 1
+        while self.i > 0:
+            if self.password[self.i] == len(self.words):
+                self.password[self.i] = 0
+                self.password[self.i - 1] += 1
             self.i -= 1
-            if self.password[0] == len(self.words):
-                self.Programm = False
-            if self.Programm != False:
-                temp = ""
-                for self.i in range(self.leght):
-                    temp += self.words[int(self.password[self.i])]
-                self.password[self.leght - 1] = int(self.password[self.leght - 1]) + 1
-                return temp
+        if self.password[0] == len(self.words):
+            self.Programm = False
+        self.temp = [self.words[i] for i in self.password]
+        self.password[-1] += 1
+        return "".join(self.temp)
