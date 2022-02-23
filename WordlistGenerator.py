73f263ba2b7f48fd2599d93463603cb074556d2d
KrouZ_CZ <64690903+KvantPro@users.noreply.github.com>
2022-02-23 21:09:23 +0300

    Update WordlistGenerator.py

--- WordlistGenerator.py
+++ WordlistGenerator.py
@@ -1,38 +1,23 @@
-#import time
-#from progress.bar import IncrementalBar
-def GenerateWords(leght, words = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"):
-    Programm = True
-    password = list("0" * leght)
-    final = []
-    len_passord = len(words) ** leght
-    #print(len_passord)
-    #bar = IncrementalBar('[*] Generate passwords', max=len_passord, suffix='%(percent).1f%%')
-    while Programm:
-        i = leght - 1
-        while i >= 0:
-            if i != 0:
-                if int(password[i]) >= len(words):
-                    password[i-1] = int(password[i-1]) + 1
-                    password[i] = 0
-            i -= 1
-            if password[0] == len(words):
-                Programm = False
-        if Programm != False:
-            temp = ""
-            for i in range(leght):
-                temp += words[int(password[i])]
-            final.append(temp)
-            password[leght - 1] = int(password[leght - 1]) + 1
-        #bar.next()
-   #bar.finish()
-    return final
-
-if __name__ == "__main__":
-    #start_time = time.time()
-    passwd = GenerateWords(4)
-    #print("--- %s seconds ---" % (time.time() - start_time))
-    #По желанию
-    #file = open("passwd.txt", "w")
-    #for item in passwd:
-    #    file.write(f"{item}\n")
-    #file.close()
+class WordlistGenerator():
+    def __init__(self, leght, words = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"):
+        self.words = words
+        self.leght = leght
+        self.password = list("0" * self.leght)
+        self.len_passord = len(self.words) ** self.leght
+        self.i = self.leght - 1
+        self.Programm = True
+    def next(self):
+        while self.i >= 0:
+            if self.i != 0:
+                if int(self.password[self.i]) >= len(self.words):
+                    self.password[self.i-1] = int(self.password[self.i-1]) + 1
+                    self.password[self.i] = 0
+            self.i -= 1
+            if self.password[0] == len(self.words):
+                self.Programm = False
+            if self.Programm != False:
+                temp = ""
+                for self.i in range(self.leght):
+                    temp += self.words[int(self.password[self.i])]
+                self.password[self.leght - 1] = int(self.password[self.leght - 1]) + 1
+                return temp
